a1238568243645bc0ed1afe58dfa3c99335ecad5
skxeve <skxeve@dummy.com>
2015-02-07 22:38:44 +0900

    add validate method.

--- Stick/lib/Validate.php
+++ Stick/lib/Validate.php
@@ -3,6 +3,29 @@ namespace Stick\lib;
 
 class Validate extends \Stick\AbstractStatic
 {
+    public static function queryToArray($query)
+    {
+        $query_sets = explode('&', $query);
+        $query_array = array();
+        foreach ($query_sets as $item) {
+            list($key, $value) = explode('=', $item, 2);
+            if (!isset($query_array[$key])) {
+                $query_array[$key] = $value;
+            } else {
+                if (is_array($query_array[$key])) {
+                    $query_array[$key][] = $value;
+                } else {
+                    $i = $query_array[$key];
+                    $query_array[$key] = array(
+                        $i,
+                        $value,
+                    );
+                }
+            }
+        }
+        return $query_array;
+    }
+
     public static function isEmpty($v)
     {
         if ($v === null || $v === '' || $v === false) {
